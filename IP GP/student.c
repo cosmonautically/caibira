70d65a5c2f4a0cd22a841a64c5987e8cc8006cb9
sabirab <sabirabibossong@gmail.com>
2025-01-27 13:38:59 +0100

    Update student.c

--- IP GP/student.c
+++ IP GP/student.c
@@ -12,11 +12,25 @@
 				passed so far in the order in which they were taken*/
 
 typedef struct studentInner * student{
+	char[10] name;
+	char[10] surname;
+	int id;
+	int year;
+	struct passedcourses{
+		char[20] course;
+		struct passedcourses* next;
+}students;
 
+struct students[100];
+int i = -1;
 
 /*EFFECTS: Creates a new student with the given data and returns it*/
 student newStudent(char * name, char * surname, int id,  int year);
-	
+	i++;
+	students[i]->name=name;
+	students[i]->surname=surname;
+	students[i]->id=id;
+	students[i]->year=year;
 
 /*EFFECT: Returns the name of the student s. 
           If s is NULL it returns "NONE".*/
@@ -47,6 +61,13 @@ int getNumberOfPassedCourses(student s);
 
 /*EFFECT: Adds courseName to the courses passed by the student s*/
 void addLastPassedCourse(student s, char* courseName);
+
+
+/*EFFECT: Frees the space used to store s. 
+          Has no effects if s is NULL.*/
+void freeStudent(student s);
+
+#endif
 
 
 /*EFFECT: Frees the space used to store s. 
