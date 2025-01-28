2bc5430e4314c4db7835b1c5bc344543c4fdc6ef
kai <humplebykai@gmail.com>
2025-01-28 17:17:43 +0100

    added LDA shtuff above student.c

--- IP GP/student.c
+++ IP GP/student.c
@@ -1,9 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #ifndef STUD
 #define STUD
 
-int* studentDirectory;
-int N;
-int size;
+
+// structure to represent a class that a student has passed.
+typedef struct Class {
+    char name[50];          // name of the class
+    struct Class* next;     // pointer to the next class in the linked list
+} Class;
+
+// structure to represent a student.
+typedef struct Student {
+    int id;                 // student ID
+    char name[10];     // first name
+    char surname[10];      // last name
+    Class* passedClasses;   // pointer to the linked list passedClasses
+} Student;
+
+// structure to manage the dynamic array of students.
+typedef struct {
+    Student* students;      // pointer to the dynamic array of Student structs
+    size_t size;            // current number of students in the array
+    size_t capacity;        // maximum number of students before resizing is needed
+} StudentDirectory;
+
+// initialize the student directory.
+void initDirectory(StudentDirectory* directory, size_t initialCapacity) {
+    directory->students = malloc(initialCapacity * sizeof(Student)); // allocate memory for the array
+    if (!directory->students) {
+        fprintf(stderr, "Error: Memory allocation failed.\n");
+        exit(EXIT_FAILURE);
+    }
+    directory->size = 0;      // initially, no students are in the directory
+    directory->capacity = initialCapacity; // set the initial capacity
+}
+
+// resize the dynamic array when it's full.
+void resizeDirectory(StudentDirectory* directory) {
+    size_t newCapacity = directory->capacity * 2; // double the capacity
+    Student* newArray = realloc(directory->students, newCapacity * sizeof(Student)); // reallocate memory
+    if (newArray) {
+        directory->students = newArray;
+        directory->capacity = newCapacity;
+    } else {
+        fprintf(stderr, "Error: Unable to resize directory\n");
+        exit(EXIT_FAILURE);
+    }
+}
 
 
 /*OVERVIEW: This is the interface for a student data structure. 
@@ -16,72 +62,27 @@ int size;
 				the names of the courses that the student 
 				passed so far in the order in which they were taken*/
 
-typedef struct studentInner{
-	char name[10];
-	char surname[10];
-	int id;
-	int year;
-	struct passedcourses{
-		char course[20];
-		struct passedcourses* next;}
-}*student;
-
-int max;//capacity of array
-int size=0;
-
-int init(int N){
-	max=N;
-	if (max>=0){
-		lda = malloc(max*sizeof(struct student));
-		return 1;}
-	else{ 
-		return 0;}
-	}
-
-int addElem(student s){
-	if (size<max){
-		lda[size]=s;
-		size++;
-		return 1;
-	}
-	else
-		return 0;
-}
-
-/* int removeElem(int i){
-	lda[i]=0;
-	for (int j=i;j<size-1;j++){
-		lda[j]=lda[j+1];
-	}
-	size--;
-return 1;
-}
-
-void print(){
-	printf("[ ");
-	for (int i=0;i<size;i++){
-		printf("%d ",lda[i]);
-		}
-	printf("]\n");
-} */
-
-void freelda(){
-	free(lda);
-}
-
 /*EFFECTS: Creates a new student with the given data and returns it*/
-student newStudent(char * name, char * surname, int id,  int year){
-	//malloc per student? or should it be for the whole array?
-	i++;
-	students[i]->name=name;
-	students[i]->surname=surname;
-	students[i]->id=id;
-	students[i]->year=year;
+ newStudent(StudentDirectory * directory, char * name, char * surname, int id,  int year){
+	if (directory->size == directory->capacity) { //check if array is full > resize
+		resizeDirectory(directory);
+	}
+
+// add student at end of array
+Student* student = &directory->students[directory->size];
+student->id = id;
+strncpy(student->name, name, sizeof(student->name) - 1);
+strncpy(student->surname, surname, sizeof(student->surname) - 1);
+student->passedClasses = NULL;
+directory->size++; //increase size 
 }
+
+
+
 
 /*EFFECT: Returns the name of the student s. 
           If s is NULL it returns "NONE".*/
-char * getStudentName(student s){
+char * getStudentName(Student s){
 	if (student[s] !=NULL){
 		return student[s]->name;
 	}
@@ -90,7 +91,7 @@ char * getStudentName(student s){
 
 /*EFFECT: Returns the surname of the student s. 
           If s is NULL it returns "NONE".*/
-char * getStudentSurname(student s){
+char * getStudentSurname(Student s){
 	if (student[s] !=NULL){
 		return student[s]->surname;
 	}
@@ -99,7 +100,7 @@ char * getStudentSurname(student s){
 
 /*EFFECT: Returns the id of the student s. 
           If s is NULL it returns -1.*/
-int getStudentId(student s){
+int getStudentId(Student s){
 	if (student[s] !=NULL){
 		return student[s]->id;}
 	else return -1;
@@ -107,7 +108,7 @@ int getStudentId(student s){
 
 /*EFFECT: Returns the year in which the student s enrolled. 
 	  If s is NULL it returns -1.*/
-int getStudentYear(student s){
+int getStudentYear(Student s){
 	if (student[s] !=NULL){
 		return student[s]->name;}
 	else return -1;
@@ -117,7 +118,7 @@ int getStudentYear(student s){
 	  the order in which they were passed (from older to newer).
 	  If s is NULL it returns NULL. 
 	  Note that the array must be deallocated by the caller.*/
-char** getPassedCourses(student s){
+char** getPassedCourses(Student s){
 	if (student[s] !=NULL){
 		return student[s]->passedcourses;}
 	else return NULL;
@@ -125,8 +126,7 @@ char** getPassedCourses(student s){
 
 /*EFFECT: Returns the number of the courses that the student passed.
 If s is NULL returns -1.*/
-
-int getNumberOfPassedCourses(student s){
+int getNumberOfPassedCourses(Student s){
 	struct passedcourses*head;
 	struct passedcourses*tracer;	
 	while (student[s]->passedcourses!=NULL){ //should it check passedcourses->tracer?
@@ -136,11 +136,25 @@ int getNumberOfPassedCourses(student s){
 }
 
 /*EFFECT: Adds courseName to the courses passed by the student s*/
-void addLastPassedCourse(student s, char* courseName);
-	//add element to end of list
+void addLastPassedCourse(Student s, char* courseName) {
+	Class* newClass = malloc(sizeof(Class));
+	if (!newClass) {
+		printf("Error: Memory allocation failed.\n");
+		exict(EXIT_FAILURE);
+	}
+	strncpy(newClass->name, courseName, sizeof(newClass->name) - 1);
+	newClass->next = Student->passedClasses;
+	Student->passedClasses = newClass;
+}
 
 /*EFFECT: Frees the space used to store s. 
           Has no effects if s is NULL.*/
-void freeStudent(student s);
+void freeStudent(Student s);
+
+
+void main () {
+
+
+}
 
 #endif
