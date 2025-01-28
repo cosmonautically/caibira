a17c263f04f6ebf8d59146f3b01a56d0065a07af
Cosmo Courtois <cosmo.courtois@gmail.com>
2025-01-28 15:08:03 +0100

    Fixed Syntax Errors With Brackets

--- IP GP/student.c
+++ IP GP/student.c
@@ -11,7 +11,7 @@
 				the names of the courses that the student 
 				passed so far in the order in which they were taken*/
 
-typedef struct studentInner * student{
+typedef struct studentInner * student {
 	char[10] name;
 	char[10] surname;
 	int id;
@@ -25,61 +25,70 @@ struct students[100];
 int i = -1;
 
 /*EFFECTS: Creates a new student with the given data and returns it*/
-student newStudent(char * name, char * surname, int id,  int year);
+student newStudent(char * name, char * surname, int id,  int year){
 	//malloc per student? or should it be for the whole array?
 	i++;
 	students[i]->name=name;
 	students[i]->surname=surname;
 	students[i]->id=id;
 	students[i]->year=year;
+}
 
 /*EFFECT: Returns the name of the student s. 
           If s is NULL it returns "NONE".*/
-char * getStudentName(student s);
-	if (student [s] !=NULL){
-		return student[s]->name;}
+char * getStudentName(student s){
+	if (student[s] !=NULL){
+		return student[s]->name;
+	}
 	else return "NONE";
+}
 
 /*EFFECT: Returns the surname of the student s. 
           If s is NULL it returns "NONE".*/
-char * getStudentSurname(student s);
+char * getStudentSurname(student s){
 	if (student [s] !=NULL){
-		return student[s]->surname;}
+		return student[s]->surname;
+	}
 	else return "NONE";
+}
 
 /*EFFECT: Returns the id of the student s. 
           If s is NULL it returns -1.*/
-int getStudentId(student s);
+int getStudentId(student s){
 	if (student [s] !=NULL){
 		return student[s]->id;}
 	else return -1;
+}
 
 /*EFFECT: Returns the year in which the student s enrolled. 
 	  If s is NULL it returns -1.*/
-int getStudentYear(student s);
+int getStudentYear(student s){
 	if (student [s] !=NULL){
 		return student[s]->name;}
 	else return -1;
+}
 
 /*EFFECT: Returns an array of the courses that the student passed in 
 	  the order in which they were passed (from older to newer).
 	  If s is NULL it returns NULL. 
 	  Note that the array must be deallocated by the caller.*/
-char** getPassedCourses(student s);
+char** getPassedCourses(student s){
 	if (student [s] !=NULL){
 		return student[s]->passedcourses;}
 	else return NULL;
+}
 
 /*EFFECT: Returns the number of the courses that the student passed.
 If s is NULL returns -1.*/
 
-int getNumberOfPassedCourses(student s);
+int getNumberOfPassedCourses(student s){
 	struct passedcourses*head;
 	struct passedcourses*tracer;	
 	while (student[s]->passedcourses!=NULL){ //should it check passedcourses->tracer?
 		//cycle through passed courses
 		int numofcourses++;}
 	return numofcourses;
+}
 
 /*EFFECT: Adds courseName to the courses passed by the student s*/
 void addLastPassedCourse(student s, char* courseName);
